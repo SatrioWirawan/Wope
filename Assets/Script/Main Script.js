c7414e7050cddf9f2e2ee5725f7f8c757ddb714c
Satrio Wirawan Utomo <73834644+SatrioWirawan@users.noreply.github.com>
2020-12-01 15:30:10 +0700

    Add files via upload

---
+++ Assets/Script/Main Script.js
@@ -0,0 +1,35 @@
+/*dropdown*/
+function myFunction() {
+    document.getElementById("dropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("dropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+  }
+
+/*photo*/
+
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+var images = document.getElementsByTagName("img")
+var modalImg = document.getElementById("img01");
+var captionTexts = document.getElementById ('alt');
+var i;
+for (i =  0; i < images.length; i++) {
+    images[i].onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        modalImg.alt = this.alt;
+        captionText.innerHTML = this.nextElementSibling.innerHTML;
+    }
+}
